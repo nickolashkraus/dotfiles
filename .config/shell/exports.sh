d347889d273f5a706c53d999b3a52a6908bc575a
nickolashkraus <0x@nickolaskraus.io>
2025-08-03 11:55:19 -0500

    Add .config/shell/exports.sh

---
+++ .config/shell/exports.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+###############################################################################
+# Shell Environment Variables
+#
+# DESCRIPTION
+#   My personal environment variables.
+#
+#   A shell environment variable is a named value that stores information the
+#   shell and programs can access. These variables affect how the shell and
+#   other programs behave, and they're available to both the current shell
+#   session and programs launched from it.
+#
+#   This file is sourced by .zshrc.
+#
+# INSTALLATION
+#   Symlink file to $XDG_CONFIG_HOME/shell/exports.sh:
+#
+#     ln -s .config/shell/exports.sh $XDG_CONFIG_HOME/shell/exports.sh
+###############################################################################
+
+# Set path to configuration files.
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+
+# Set language.
+export LANG=en_US.UTF-8
+
+# Set preferred editor for local and remote sessions.
+export EDITOR='vim'
+
+# Set path to kubeconfig file (prepend Workiva-specific kubeconfig file).
+export KUBECONFIG=$HOME/.kube/workiva.yaml:$HOME/.kube/config
+
+# Set default AWS profile.
+export AWS_PROFILE=master.root
