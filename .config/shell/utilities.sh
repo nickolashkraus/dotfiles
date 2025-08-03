83fc998a261699eaf2fdbc068edaaa89a3da66ca
nickolashkraus <0x@nickolaskraus.io>
2025-08-03 12:04:03 -0500

    Add .config/shell/utilities.sh

---
+++ .config/shell/utilities.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+###############################################################################
+# Shell Utilities Configuration
+#
+# DESCRIPTION
+#   My personal shell utilities configuration.
+#
+#   A shell utility is a standalone command-line program that extends your
+#   shell's capabilities beyond the built-in commands. Unlike aliases and
+#   functions (which are shell features), utilities are separate executable
+#   programs that you install and run from your shell.
+#
+#   This file is sourced by .zshrc.
+#
+# INSTALLATION
+#   Symlink file to $XDG_CONFIG_HOME/shell/utilities.sh:
+#
+#     ln -s .config/shell/utilities.sh $XDG_CONFIG_HOME/shell/utilities.sh
+###############################################################################
