21080b89e55f2559fdae7c4dee5b39bf79c050be
nickolashkraus <0x@nickolaskraus.io>
2025-08-03 12:00:56 -0500

    Add .config/shell/functions.sh

---
+++ .config/shell/functions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+###############################################################################
+# Shell Functions
+#
+# DESCRIPTION
+#   My personal shell functions.
+#
+#   A shell function is a reusable block of code that you can define once and
+#   then call multiple times by name. Functions are more powerful than aliases
+#   because they can accept parameters, contain complex logic, and span
+#   multiple lines.
+#
+#   This file is sourced by .zshrc.
+#
+# INSTALLATION
+#   Symlink file to $XDG_CONFIG_HOME/shell/functions.sh:
+#
+#     ln -s .config/shell/functions.sh $XDG_CONFIG_HOME/shell/functions.sh
+###############################################################################
