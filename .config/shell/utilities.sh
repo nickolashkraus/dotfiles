bc236968ed0b502138ceeaaf84a967fab3df6608
nickolashkraus <0x@nickolaskraus.io>
2025-08-04 12:26:01 -0500

    Add fzf configuration

--- .config/shell/utilities.sh
+++ .config/shell/utilities.sh
@@ -47,6 +47,93 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 source "$HOME/.vim/bundle/gruvbox/gruvbox_256palette.sh"
 
 ###############################################################################
+# fzf
+#
+# fzf is a general-purpose command-line fuzzy finder. It helps you quickly
+# search and select items from a list using a fast, interactive fuzzy search
+# interface. It's written in Go and works in any Unix-like terminal
+# environment.
+#
+# See:
+#   - https://github.com/junegunn/fzf
+#   - https://junegunn.github.io/fzf
+###############################################################################
+
+# Set up fzf shell integration.
+#
+# Key Bindings:
+#   Ctrl - r: Paste the selected command from history onto the command line.
+#   Ctrl - t: Paste the selected files and directories onto the command line.
+#   Alt - c: cd into the selected directory.
+source <(fzf --zsh)
+
+# Default fzf command (uses ripgrep).
+#   --hidden: Search hidden files and directories.
+#   --follow: Follow symbolic links while traversing directories.
+#   --glob: Include or exclude files and directories that match the given glob.
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!node_modules/*"'
+
+# Use 'FZF_DEFAULT_COMMAND' for Ctrl - t.
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Default fzf options.
+export FZF_DEFAULT_OPTS=' --height 40% --preview-window right:50%'
+
+# Additional options for Ctrl - r.
+#
+# Press Ctrl - y to copy the selected command to the system clipboard.
+export FZF_CTRL_R_OPTS="
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press Ctrl - y to copy the selected command to the system clipboard.'
+"
+
+# Additional options for Ctrl - t.
+#
+# Press Ctrl - / to toggle file preview.
+export FZF_CTRL_T_OPTS="
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(hidden|)'
+  --color header:italic
+  --header 'Press Ctrl - / to toggle file preview.'
+"
+
+# Additional options for Alt - c.
+export FZF_ALT_C_OPTS="
+  --preview 'tree -C {}'
+"
+
+# Configure fzf theme.
+#
+#   fg:      #ebdbb2 - fg1
+#   fg+:     #d5c4a1 - fg2
+#   bg:      #282828 - bg0
+#   bg+:     #3c3836 - bg1
+#   hl:      #fabd2f - yellow (bright)
+#   hl+:     #d79921 - yellow (neutral)
+#   info:    #665c54 - bg3
+#   marker:  #83a598 - blue (bright)
+#   prompt:  #665c54 - bg3
+#   spinner: #665c54 - bg3
+#   pointer: #83a598 - blue (bright)
+#   header:  #665c54 - bg3
+#   border:  #3c3836 - bg1
+#   label:   #665c54 - bg3
+#   query:   #ebdbb2 - fg1
+#
+# See:
+#   - https://github.com/junegunn/fzf/wiki/Color-schemes
+#   - https://vitormv.github.io/fzf-themes
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:#ebdbb2,fg+:#d5c4a1,bg:#282828,bg+:#3c3836
+  --color=hl:#fabd2f,hl+:#d79921,info:#665c54,marker:#83a598
+  --color=prompt:#665c54,spinner:#665c54,pointer:#83a598,header:#665c54
+  --color=border:#3c3836,label:#665c54,query:#ebdbb2
+  --border="rounded" --border-label="" --preview-window="border-rounded" --prompt="> "
+  --marker=">" --pointer="▌" --separator="─" --scrollbar="│"
+  --layout="reverse"'
+
+###############################################################################
 # Go
 ###############################################################################
 
