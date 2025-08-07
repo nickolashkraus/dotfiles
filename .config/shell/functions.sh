ecd77751e43847342daa796f240ed40072fcf157
nickolashkraus <0x@nickolaskraus.io>
2025-08-07 11:26:29 -0500

    Add .config/shell/functions.sh

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -17,3 +17,22 @@
 #
 #     ln -s .config/shell/functions.sh $XDG_CONFIG_HOME/shell/functions.sh
 ###############################################################################
+
+# See: https://junegunn.github.io/fzf/tips/ripgrep-integration/
+rfv() (
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            vim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            vim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  fzf --disabled --ansi --multi --tmux \
+    --bind "start:$RELOAD" --bind "change:$RELOAD" \
+    --bind "enter:become:$OPENER" \
+    --bind "ctrl-o:execute:$OPENER" \
+    --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+    --delimiter : \
+    --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+    --preview-window '~4,+{2}+4/3,<80(up)' \
+    --query "$*"
+)
